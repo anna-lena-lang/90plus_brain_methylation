2c58298b590c1aa9f9318663c32a1e1e979115c1
anna-lena-lang <107249982+anna-lena-lang@users.noreply.github.com>
2022-11-07 20:41:33 +0100

    Update SuppFig_8_DG_neuron_gamma_secretase_niaaaascore.R

--- figures_and_tables/SuppFig_8_DG_neuron_gamma_secretase_niaaaascore.R
+++ figures_and_tables/SuppFig_8_DG_neuron_gamma_secretase_niaaaascore.R
@@ -1,4 +1,4 @@
-## Plot SuppFig 7, Figure of neurons in the dentate gyrus, gamma secretase
+## Plot SuppFig 8, Figure of neurons in the dentate gyrus, gamma secretase
 
 library(cowplot)
 library(magrittr)
